5dd5b9195cece5d25ac3eeb20af365c2e688fdf9
Dmitry Dolzhenko <mailbox@dolzhenko.me>
2022-12-06 22:07:13 +0100

    .emacs.d: bootstrap GTD workflow with org mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -182,3 +182,76 @@
 ;;
 
 (use-package magit)
+
+;;
+;; Org
+;;
+(require 'org)
+(require 'org-mac-link)
+
+(setq org-directory "~/Documents/org")
+
+(setq org-agenda-files (list "inbox.org" "agenda.org" "projects.org"))
+(setq org-agenda-hide-tags-regexp ".")
+(setq org-agenda-prefix-format '((agenda . " %i %-12:c%?-12t% s")
+                                 (todo   . " %i %-12:c")
+                                 (tags   . " %i %-12:c")
+                                 (search . " %i %-12:c")))
+(setq org-agenda-window-setup 'only-window)
+
+(setq org-refile-targets
+      '(("projects.org" :regexp . "\\(?:\\(?:Note\\|Task\\)s\\)")))
+
+(setq org-refile-use-outline-path 'file)
+(setq org-outline-path-complete-in-steps nil)
+
+(setq org-capture-templates
+      `(("i" "Inbox" entry (file "inbox.org")
+         ,(concat "* TODO %?\n"
+                  "/Entered on/ %U"))
+        ("m" "Meeting" entry (file+headline "agenda.org" "Future")
+         ,(concat "* %? :meeting:\n"
+                  "<%<%Y-%m-%d %a %H:00>>"))
+        ("n" "Note" entry (file "notes.org")
+         ,(concat "* Note (%a)\n"
+                  "/Entered on/ %U\n" "\n" "%?"))))
+
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "NEXT(n)" "HOLD(h)" "|" "DONE(d)")))
+
+(setq org-log-done 'time)
+
+(defun x/org-capture-inbox ()
+     (interactive)
+     (call-interactively 'org-store-link)
+     (org-capture nil "i"))
+
+(defun x/org-save-agenda-buffers ()
+  "Save `org-agenda-files' buffers without user confirmation."
+  (interactive)
+  (message "Saving org-agenda-files buffers...")
+  (save-some-buffers t (lambda ()
+                         (when (member (buffer-file-name) org-agenda-files)
+                           t)))
+  (message "Saving org-agenda-files buffers... done"))
+
+(defun x/org-log-next-creation-date (&rest ignore)
+  "Log NEXT creation time in the property drawer under the key 'ACTIVATED'"
+  (when (and (string= (org-get-todo-state) "NEXT")
+             (not (org-entry-get nil "ACTIVATED")))
+    (org-entry-put nil "ACTIVATED" (format-time-string "[%Y-%m-%d]"))))
+
+(define-key global-map (kbd "C-c i") 'x/org-capture-inbox)
+(define-key global-map (kbd "C-c c") 'org-capture)
+(define-key global-map (kbd "C-c a") 'org-agenda)
+(define-key global-map (kbd "C-c g") 'org-mac-link-get-link)
+
+;; Use the full window instead of splitting the current one
+(add-hook 'org-capture-mode-hook 'delete-other-windows)
+
+(add-hook 'org-after-todo-state-change-hook #'x/org-log-next-creation-date)
+
+;; Add it after refile
+(advice-add 'org-refile :after
+            (lambda (&rest _)
+              (x/org-save-agenda-buffers)))
