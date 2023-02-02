3dc9301fc4ea74e39215262b5df31f8c15702d99
Dmitry Dolzhenko <mailbox@dolzhenko.me>
2023-02-02 10:08:31 +0100

    .emacs.d: add projectile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -228,6 +228,11 @@
 (use-package which-key
   :config (which-key-mode))
 
+(use-package projectile
+  :config (projectile-mode)
+  :bind-keymap
+  ("C-c p" . projectile-command-map))
+
 ;;
 ;; Org
 ;;
