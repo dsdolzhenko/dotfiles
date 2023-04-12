bffb961fc96b1ed706da25766d5f78bcf58ba3ea
Dmitry Dolzhenko <mailbox@dolzhenko.me>
2023-04-12 12:12:24 +0200

    scripts/setup-emacs.sh: setup command line aliases for emacs

--- scripts/setup-emacs.sh
+++ scripts/setup-emacs.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
+echo "Linking .emacs.d directory..."
 ln -sfh "${PWD}/.emacs.d" "${HOME}/.emacs.d"
 
+echo "Installing Fira Code fonts..."
 NONINTERACTIVE=1 brew tap --quiet homebrew/cask-fonts && \
     brew install --quiet --cask font-fira-code
+
+if ! alias emacs >/dev/null 2>&1 ; then
+    echo "Setup aliases for emacs.."
+    cat <<EOF >> "${HOME}/.zshenv"
+# Emacs
+alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs"
+alias ec="emacs -n"
+EOF
+fi
