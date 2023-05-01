9dd788f81186a2efea0623757ab2860acac80b32
Dmitry Dolzhenko <mailbox@dolzhenko.me>
2023-05-01 14:49:27 +0200

    .emacs.d: add x/dired-view-file-other-window

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -209,9 +209,24 @@
 ;;
 ;; Dired
 ;;
+(require 'dired)
 
 ;; MacOS built-in ls command doesn't support "--dired" option
 (setq dired-use-ls-dired nil)
+
+(defun x/dired-view-file-other-window ()
+  "In Dired, examine a file in view mode, returning to Dired when done.
+When file is a directory, show it in this buffer if it is inserted.
+Otherwise, display it in another buffer."
+  (interactive)
+  (let ((file (dired-get-file-for-visit)))
+    (if (file-directory-p file)
+        (or (and (cdr dired-subdir-alist)
+                 (dired-goto-subdir file))
+            (dired file))
+      (view-file-other-window file))))
+
+(define-key dired-mode-map (kbd "v") 'x/dired-view-file-other-window)
 
 ;;
 ;; vterm
