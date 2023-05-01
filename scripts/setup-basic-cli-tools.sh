29de083dc9630774559c2185285b51d679105c00
Dmitry Dolzhenko <mailbox@dolzhenko.me>
2023-05-01 14:50:41 +0200

    scripts/setup-basic-cli-tools.sh: fzf config

--- scripts/setup-basic-cli-tools.sh
+++ scripts/setup-basic-cli-tools.sh
@@ -3,3 +3,6 @@
 NONINTERACTIVE=1 brew install wget jq fzf fd git
 
 /usr/local/opt/fzf/install --all --no-bash --no-fish
+
+echo -e "\n# fzf" >> ~/.zshenv
+echo -e "export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'" >> ~/.zshenv
